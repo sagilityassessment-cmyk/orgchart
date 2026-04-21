e08ff31377e31e5c3fca6e65970863d06e64bb43
sagilityph <sagilityassessment@gmail.com>
2026-04-22 03:28:35 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,12 @@
-function login(){const u=document.getElementById("username").value;const p=document.getElementById("password").value;if(u==="SagilityIloilo"&&p==="Sagility_1"){window.location="dashboard.html";}else{document.getElementById("error").innerText="Invalid username or password";}}
+function login() {
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+
+  if (user === "SagilityIloilo" && pass === "Sagility_1") {
+    window.location.href = "dashboard.html";
+  } else {
+    document.getElementById("error").innerText =
+      "Invalid username or password";
+  }
+}
+``
